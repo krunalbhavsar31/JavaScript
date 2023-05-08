b76542b090a1dac7e8e870e756fcea869fa3f397
krunalbhavsar31 <krunal93bhavsar@gmail.com>
2023-05-08 17:13:21 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function multiply()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    result = document.getElementById("result").innerHTML = num1 * num2;
+    console.log(result);
+}
+
+// function divideBy() 
+// { 
+//     num1 = document.getElementById("firstNumber").value;
+//     num2 = document.getElementById("secondNumber").value;
+//     document.getElementById("result").innerHTML = num1 / num2;
+// }
